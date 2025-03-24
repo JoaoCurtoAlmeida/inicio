0b290317ae11c283e63732d4317267de7f2f3ee5
Joao Victor <joao.curto.almeida@example.com>
2025-03-24 17:12:10 -0300

    feito o projetos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 import { Page1Component } from "./page1/page1.component";
 import { InicioComponent } from './inicio/inicio.component';
 import { Page2Component } from "./page2/page2.component";
+import { ProjetosComponent } from './projetos/projetos.component';
 @Component({
   selector: 'app-root',
-  imports: [Page1Component, InicioComponent, Page2Component],
+  imports: [Page1Component, InicioComponent, Page2Component, ProjetosComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
