61e08bf724edf1dd52855645ebe65a61e5c96163
Joao Victor <joao.curto.almeida@example.com>
2025-03-31 07:49:30 -0300

    finalizado

--- src/app/projetos/projetos.component.ts
+++ src/app/projetos/projetos.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2, OnInit, OnDestroy, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-projetos',
@@ -6,6 +6,28 @@ import { Component } from '@angular/core';
   templateUrl: './projetos.component.html',
   styleUrl: './projetos.component.css'
 })
+
 export class ProjetosComponent {
+  private isInfoMode = false;
+
+  constructor(private renderer: Renderer2, private el: ElementRef) { }
+
+
+   Oninfo() {
+    const myDivInfo = this.el.nativeElement.querySelector('.container-info');
+
+
+    this.isInfoMode = !this.isInfoMode;
+    if (this.isInfoMode) {
+      this.renderer.addClass(myDivInfo, 'on-info');
+
+
+    } else {
+
+      this.renderer.removeClass(myDivInfo, 'on-info');
+
+
+    }
+  }
 
 }
